7d4a6c6e3c1bf1c5d92aa6a94a88ff2c44168ffd
nidhin cn <nidhincareer@gmail.com>
2023-04-16 22:56:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
